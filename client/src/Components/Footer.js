fb348f2e2666ce5462212621b916925f8c8ce624
amine-99404 <76427477+amine-99404@users.noreply.github.com>
2021-04-09 10:13:43 +0100

    first commit

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const footer = () => {
+    return (
+    <div className="mt-2" >
+            <footer>
+      <p>
+        <a href="https://github.com/amine-99404">Amine saadaoui</a> 
+        <p> © 2021 All rights reserved.</p>
+      </p>
+      <p> welcome to my shope</p>
+     
+    </footer>
+    </div>
+    )
+}
+
+export default footer
